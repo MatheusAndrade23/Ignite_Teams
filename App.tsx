43ff247dd0ecaf835239d554a8f48bf4e430fc65
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-12-22 21:38:39 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "react-native";
+import { Groups } from "@screens/Groups";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Groups />
+    </>
+  );
+}
